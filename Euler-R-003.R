7a3e42d98cc07b27a1a8960964d0049c20b0fc10
ntekal <ntekal@gmail.com>
2018-01-05 21:18:26 -0600

    completed 3, runs but needs to be optimized

--- Euler-R-003.R
+++ Euler-R-003.R
@@ -1,4 +1,4 @@
-#PSEUDO
+#PSEUDO (SIEVE OR ERASTOTHENES)
 # Input: an integer n > 1.
 # 
 # Let A be an array of Boolean values, indexed by integers 2 to n,
@@ -10,3 +10,39 @@
 #   A[j] := false.
 # 
 # Output: all i such that A[i] is true.
+
+sieve <- function(n) {
+  if (n <= 1) {
+    return(numeric(0))
+  }
+  if (n == 2) {
+    return(c(2))
+  }
+  
+  a <- seq(2,n)
+  b <- c()
+  b[1] <- 2
+  
+  i <- 1
+  while (length(a) > 0) {
+    b[i] <- a[1]
+    i <- i + 1
+    a <- a[!(a %% a[1] == 0)]
+  }
+  b
+}
+
+pfactor <- function(x) {
+  factors <- c()
+  j <- 1
+  primes <- sieve( floor(sqrt(x)) )
+  for (prime in primes) {
+    if (x %% prime == 0) {
+      factors[j] <- prime
+      j <- j + 1
+    }
+  }
+  factors
+}
+
+pfactor(600851475143)
